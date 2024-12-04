c5b489cbf3ca957546e7472da5bddd771215f0c9
Fevenir <65770420+ChitrakshTarun@users.noreply.github.com>
2024-12-04 22:22:16 +0530

    refactor(db): update getAllAlbums orderby filters
    first entry inside artist_name, sorted in ascending irrespective of character casing

--- db/actions/albumsAction.ts
+++ db/actions/albumsAction.ts
@@ -1,6 +1,6 @@
 "use server";
 import { db } from "@/db/drizzle";
-import { asc } from "drizzle-orm";
+import { asc, sql } from "drizzle-orm";
 import { albums } from "@/db/schema";
 
 export const getAllAlbums = async () => {
@@ -14,7 +14,7 @@ export const getAllAlbums = async () => {
       artworkUrl: albums.artworkUrl,
     })
     .from(albums)
-    .orderBy(asc(albums.artistName), asc(albums.releaseDate));
+    .orderBy(sql`lower(${albums.artistName}[1])`, asc(albums.releaseDate));
   return data.map((album) => ({
     ...album,
     artistName: album.artistName as string[],
